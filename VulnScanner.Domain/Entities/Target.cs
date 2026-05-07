1611ec518925f68990d910b14df2c444f5c6a98c
andrew-ltu <21715223@students.latrobe.edu.au>
2026-05-07 21:59:17 +1000

    Add Target entity with properties

---
+++ VulnScanner.Domain/Entities/Target.cs
@@ -0,0 +1,11 @@
+namespace VulnScanner.Domain.Entities;
+ 
+public class Target
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
+ 
